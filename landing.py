27550203c877076381be2617ed24390e68eef91f
Eamon Saeid <eamonsaeid@msn.com>
2026-09-04 10:49:36 +0100

    Landing updated to extract all previous years mens singles

--- landing.py
+++ landing.py
@@ -2,6 +2,7 @@ import requests
 import logging
 import uuid
 import json
+import re
 from datetime import datetime
 from google.cloud import storage
 
@@ -17,63 +18,74 @@ client=storage.Client()
 bucket=client.bucket('tennisdataengproject-landing')
 
 
-
-# Get modified time and urls for all the different files
+ 
+# Get modified time and urls for all the available files
 r=requests.get('https://stats.tennismylife.org/api/data-files', timeout=10)
-latest_2026_file_from_source=next(i for i in r.json()['files'] if "2026" in i['name'])
-last_modified=latest_2026_file_from_source['mtime']
-csv_url=latest_2026_file_from_source['url']
+
+# Pulling only the singles matches from 1970 to now 
+files=[i for i in r.json()['files'] if re.fullmatch(r'\d{4}\.csv',i['name'])]
 
 # Variables needed in ingestion
 ingest_datetime=datetime.now()
 ingest_date=ingest_datetime.strftime("%Y-%m-%d")
 batch_id=str(uuid.uuid4())
-data_path=f"raw/tml/matches/ingest_date={ingest_date}/batch_id={batch_id}/2026.csv"
-
 
 # First checking GCP to see when the last download was done/if there is a latest mtime
 blobs=bucket.list_blobs(prefix='raw/tml/matches')
 manifest_blobs=[i for i in blobs if i.name.endswith("_manifest.json")]
-if not manifest_blobs:
-    logger.info("No previous manifest found - First run")
-    last_seen_mtime=None
-else:
-    latest=max(manifest_blobs, key=lambda x: x.time_created)
-    last_seen_mtime=json.loads(latest.download_as_text())['source_version']
+
+for file in files:
+    last_modified=file['mtime']
+    csv_url=file['url']
+    file_name=file['name']
+    data_path=f"raw/tml/matches/ingest_date={ingest_date}/batch_id={batch_id}/file={file_name}/{file_name}"
+    file_manifest_blobs=[i for i in manifest_blobs if i.name.endswith("_manifest.json") and f"file={file_name}" in i.name]
+
+    if not file_manifest_blobs:
+        logger.info(f"No previous manifest found for {file_name} - First run")
+        last_seen_mtime=None
+    else:
+        latest=max(file_manifest_blobs, key=lambda x: x.time_created)
+
+        # Learning point - When I expanded this code from pulling a single file, to then pulling all the files one after the other, I hit a failure
+        # type called "time of check to time of use". I went into the console and delted some of the previous runs, so the blob had been edited wehn I came to it
+        # Generally this error won't be enountered but getting the latest_live version protects againts this sort of error, so why not
+        # The fix is to use the manifest_blobs list to determine the 
+        latest_live=bucket.blob(latest.name)
+        last_seen_mtime=json.loads(latest_live.download_as_text())['source_version']
 
 
+    if last_seen_mtime is None or last_modified>last_seen_mtime:
+
+        # Downloading the CSV
+        response=requests.get(csv_url, timeout=30)
+        response.raise_for_status()
+        csv_bytes=response.content
+
+        # row_count
+        csv_row_count=len(csv_bytes.decode("utf_8").splitlines())-1
 
 
-if last_seen_mtime is None or last_modified>last_seen_mtime:
-
-    # Downloading the CSV
-    response=requests.get(csv_url, timeout=30)
-    response.raise_for_status()
-    csv_bytes=response.content
-
-    # row_count
-    csv_row_count=len(csv_bytes.decode("utf_8").splitlines())-1
+        # constants
+        source_name='tml'
+        entity='matches'
 
 
-    # constants
-    source_name='tml'
-    entity='matches'
+        manifest_dict={
+            "source": source_name,
+            "entity": entity,
+            "file_name":file_name,
+            "extraction_window":{'from':ingest_date, 'to':ingest_date},
+            "row_count":csv_row_count,
+            'source_version':last_modified,
+            'ingested_at':ingest_datetime.isoformat(),
+            'batch_id':batch_id
+        }
 
+        # Uploading the csv onto the GCP bucket
+        manifest_path=f"raw/tml/matches/ingest_date={ingest_date}/batch_id={batch_id}/file={file_name}/_manifest.json"
+        manifest_blob=bucket.blob(manifest_path)
+        manifest_blob.upload_from_string(json.dumps(manifest_dict, indent=2), content_type='application/json')
 
-    manifest_dict={
-        "source": source_name,
-        "entity": entity,
-        "extraction_window":{'from':ingest_date, 'to':ingest_date},
-        "row_count":csv_row_count,
-        'source_version':last_modified,
-        'ingested_at':ingest_datetime.isoformat(),
-        'batch_id':batch_id
-    }
-
-    # Uploading the csv onto the GCP bucket
-    manifest_path=f"raw/tml/matches/ingest_date={ingest_date}/batch_id={batch_id}/_manifest.json"
-    manifest_blob=bucket.blob(manifest_path)
-    manifest_blob.upload_from_string(json.dumps(manifest_dict, indent=2), content_type='application/json')
-
-    csv_blob=bucket.blob(data_path)
-    csv_blob.upload_from_string(csv_bytes, content_type="text/csv")
+        csv_blob=bucket.blob(data_path)
+        csv_blob.upload_from_string(csv_bytes, content_type="text/csv")
